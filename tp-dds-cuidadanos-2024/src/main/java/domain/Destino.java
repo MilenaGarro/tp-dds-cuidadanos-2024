6aaeabc0b6fb93bea9d96d44096a809d70df7025
Milena <mgarro@frba.utn.edu.ar>
2024-04-30 21:41:37 -0300

    Agrego Clase Destino
    Modifico Transeunte para que coincidan los parametros de los metodos, el constructor de destino y agrego el boceto del diagrama de clases actualizado

--- tp-dds-cuidadanos-2024/src/main/java/domain/Destino.java
+++ tp-dds-cuidadanos-2024/src/main/java/domain/Destino.java
@@ -1,16 +1,75 @@
 package domain;
 
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.TravelMode;
+
 public class Destino {
-    private String puntoDePartida;
+    private String direccionActual;
     private String puntoDeLlegada;
 
+    private Integer numeroDeParada;
 
-    public java.lang.Float calcularDistancia(String puntoDePartida, String puntoDeLlegada) {
+    private float tiempoDeDemora;
 
-        return null;
+    private float distancia = 0;
+
+    private String apikey = "AIzaSyD-8Jm0J6Q1J9QK5jJj6QJ6QJ6QJ6QJ6QJ"; // clave de la API de Google Maps anda a chequearlo sabes a donde
+
+    public Destino(Integer numeroDeParada) {
+        this.numeroDeParada = numeroDeParada;
     }
 
-    public java.lang.Float calcularTiempoDeDemora(String puntoDePartida, String puntoDeLlegada) {
-        return null;
+    public float calcularTiempoDeDemora(String direccionActual, String puntoDeLlegada, Integer tiempoXParada) {
+        this.tiempoDeDemora = getTravelTime(this.apikey, direccionActual, puntoDeLlegada) + tiempoXParada;
+        return this.tiempoDeDemora;
+    }
+
+    public float calcularDistancia(String direccionActual, String puntoDeLlegada) {
+        try {
+            GeoApiContext context = new GeoApiContext.Builder()
+                    .apiKey(this.apikey)
+                    .build();
+
+            DistanceMatrix result = DistanceMatrixApi.newRequest(context)
+                    .origins(direccionActual)
+                    .destinations(puntoDeLlegada)
+                    .mode(TravelMode.WALKING)
+                    .await();
+
+
+            String distanceText = result.rows[0].elements[0].distance.humanReadable;
+            float distanceFloat = Float.parseFloat(distanceText.replaceAll("[^\\d.]", ""));
+            return distanceFloat;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+    public static float getTravelTime(String apiKey, String origin, String destination) {
+        try {
+            // falta tener la clave api ???
+            GeoApiContext context = new GeoApiContext.Builder()
+                    .apiKey(apiKey)
+                    .build();
+
+            // Realiza la solicitud a la API de Distance Matrix
+            DistanceMatrix result = DistanceMatrixApi.newRequest(context)
+                    .origins(origin)
+                    .destinations(destination)
+                    .mode(TravelMode.WALKING)
+                    .await();
+
+            long durationInSeconds = result.rows[0].elements[0].duration.inSeconds;
+
+            // Convierte la duración de segundos a minutos como valor flotante
+            float durationInMinutes = durationInSeconds / 60.0f;
+
+            return durationInMinutes;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
 }
--- tp-dds-cuidadanos-2024/src/main/java/domain/Transeunte.java
+++ tp-dds-cuidadanos-2024/src/main/java/domain/Transeunte.java
@@ -1,5 +1,7 @@
 package domain;
 import java.lang.Float;
+import domain.Destino;
+import java.util.Date;
 
 public class Transeunte {
     String puntoDePartida;
@@ -12,11 +14,11 @@ public class Transeunte {
 
     public void iniciarViaje() {
         Date tiempoahora = new java.util.Date();
-        Destino destino = new Destino();
+        Destino destino = new Destino(1);
         float tiempoTotal;
         tiempoDeDemora = destino.calcularDistancia(puntoDePartida, puntoDeLlegada);
         tiempoTotal = tiempoDeDemora - tiempoTotal;
-        distancia = destino.calcularTiempoDeDemora(puntoDePartida, puntoDeLlegada);
+        distancia = destino.calcularTiempoDeDemora(puntoDePartida, puntoDeLlegada, 0);
         this.chequearTiempoDeDemora(tiempoahora, tiempoDeDemora);
     }
 
